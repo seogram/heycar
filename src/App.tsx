d0011b7ac651ade654b82b795b434e03dc78477e
Afshin <{ID}+{username}@users.noreply.github.com>
2022-10-15 12:20:20 +0200

    feat(HC-1):Add required scaffold including Redux , StyledComponent

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import styled from "styled-components";
+
+
+const Title = styled.h2`
+color : blue;
+`
+function App() {
+
+  return (
+<>
+<div>
+ <Title>App </Title>
+  </div>
+</>
+  );
+}
+
+export default App;
